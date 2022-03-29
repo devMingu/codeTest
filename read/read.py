a546abaa5508d061fb3b024e77f7da59b617c8d6
user19971212 <rooster100@naver.com>
2022-03-29 22:41:43 +0900

    CODING TEST

---
+++ read/read.py
@@ -0,0 +1,34 @@
+# 그 첫번째로 PROBLEM SOLVING STRATEGIES(문제풀이전략)을 배웁니다.
+#
+#     1. Understand Problem(문제를 이해하기)
+#     2. Explore Concrete Examples(구체적 예시 탐색)
+#     3. Break It Down(쪼개기)
+#     4. Solve/Simplify(풀고/ 단순화하고)
+#     5. Look Back and Refacor(다시 돌아보면서 확인해서 정리하기)
+#
+# 1. Understand Problem(문제의 이해)
+#     -Can I restate the problem in my own words?(문제를 자신만의 표현으로 이해할수있나요?)
+#     -What are the inputs that go into the problem(문제에서 입력하라는 입력값이 무엇인가요?)
+#     -What are the outputs that should come from the solution to the problem?(문제해결에 있어 출력하라는 출력값이 무엇인가요?)
+#     -Can the ouputs be determineded from the inputs?(출력값이 입력값에 의해 결정되나요?)
+#     -How should i label the important pieces of data that are a part of the problem?(문제의 일부인 데이터의 중요한 부분을 어떻게 이름을 붙여야할까?(불러야할까?))
+# 2. Explore Concrete Examples(구체적 예시 탐색)
+#     -Coming up with examples can help you understanding problem better(예시를 떠올리는 것은 도움이 된다)
+#     -Example also provide sanity checks that your eventual solution works how it should(예시는 최종답안을 확인할 수 있도록 도와줍니다)
+#     -Start with Simple Examples(간단한 예시부터 시작합니다)
+#     -Progress to More Complex Examples(더 복잡한 예시로 넘어갑니다)
+#     -Explore Example with Empty Inputs(비어있는 입력값으로 가지는 예시들을 탐색하는 것은 도움이됩니다)
+#     -Explore Examples with Invalid Inputs(유효하지 않은 입력값을 탐색하는것은 도움이됩니다)
+# 3.Break It Down(쪼개기)
+#     -Explicity write out the steps you need to take(내가 취해야 하는 단계들을 직접 적어보기)
+# 4.Solve/Simplify(풀고/ 단순화하고)
+#     -Solve the problem or solve a simpler problem(문제를 해결할수있다면 해결하고 그렇지 못하다면 어려운 부분은 제쳐두고 할줄아는것부터 해라)
+#     -어려운 부분을 만난다면 일시적으로 어려운문제는 제쳐두고 간단한 문제를 적어두고 그 다음 어려운 부분을 통합해라
+# 5. Look Back and Refactor(다시 돌아보면서 확인해서 정리하기)
+#     -Can you check the result? (출력값을 확인할수있나요?)
+#     -Can you derive result differently?(출력값을 다르게 도출할 수도 있나요?)
+#     -Can you understand it an glance?(눈으로 살펴봤을때 확인하고 이해할수있나요?)
+#     -Can you use the result or method for some other problem?(결과값이나 메소드를 다른 문제해결에도 사용할수있나요?)
+#     -Can you improve the performance of your solution?(어떻게 하면 나의 해결방안의 성능을 높일수있을까요?)
+#     -Can you think of other ways to refactor?
+#     -How have other people solved this problem?
