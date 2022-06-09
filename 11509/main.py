582c5b34314396c48f4462bf460a720480c41ef3
devMingu <rooster100@naver.com>
2022-06-09 14:32:02 +0900

    알고리즘 풀이

--- 11509/main.py
+++
@@ -1,8 +0,0 @@
-n, k = map(int, input().split())
-kids = [int(x) for x in input().split()]
-start = 0
-last = 1
-answer = 0
-arr = []
-for i in range(1, n):
-    arr.append(kids[i] - kod)
--- leetcode/main.py
+++
@@ -1,6 +0,0 @@
-a = {
-    'mingu': [0, 'b', 'c', 'd', 'd', 'd', 'c']
-}
-
-for key, value in a.items():
-    print(key, value)
