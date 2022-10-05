1b00ac04646e96d75a1ef35c4c88a58d82024b92
devMingu <rooster100@naver.com>
2022-10-05 14:09:39 +0900

    알고리즘 풀이

---
+++ 백준/점프 점프.py
@@ -0,0 +1,27 @@
+import sys
+sys.setrecursionlimit(10000)
+
+def dfs(x):
+    if x <= -1 or x >= n:
+        return False
+
+    if visited[x] == False:
+        visited[x] = True
+        dfs(x - arr[x])
+        dfs(x + arr[x])
+        return True
+
+    return False
+
+
+
+n = int(input())
+arr = [int(x) for x in input().split()]
+s = int(input())
+
+visited = [False] * n
+dfs(s-1)
+answer = visited.count(True)
+print(answer)
+
+
