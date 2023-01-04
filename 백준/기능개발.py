0374907fe70f7a817ad20c2d3eccd8ef111201b1
devMingu <rooster100@naver.com>
2023-01-04 12:18:18 +0900

    알고리즘 풀이

--- 백준/기능개발.py
+++ 백준/기능개발.py
@@ -19,3 +19,4 @@ def solution(progresses, speeds):
 
 print(solution([93,30,55], [1,30,5]))
 print(solution([95, 90, 99, 99, 80, 99], [1,1,1,1,1,1]))
+
--- 백준/바이러스.py
+++ 백준/바이러스.py
@@ -1,28 +1,61 @@
-from collections import deque
+# from collections import deque
+#
+# def bfs(computer, start, visited):
+#     queue = deque([start])
+#     visited[start] = True
+#     while queue:
+#         v = queue.popleft()
+#         visited[v] = True
+#         for i in computer[v]:
+#             if not visited[i]:
+#                 queue.append(i)
+#                 visited[i] = True
+#
+# n = int(input())
+# m = int(input())
+# computer = [[] for _ in range(n+1)]
+#
+# visited = [False] * (n+1)
+#
+# for i in range(m):
+#     a, b = map(int, input().split())
+#     computer[a].append(b)
+#     computer[b].append(a)
+# bfs(computer, 1, visited)
+#
+# print(visited.count(True) - 1)
+#
+#
+#
 
-def bfs(computer, start, visited):
-    queue = deque([start])
-    visited[start] = True
-    while queue:
-        v = queue.popleft()
-        visited[v] = True
-        for i in computer[v]:
-            if not visited[i]:
-                queue.append(i)
-                visited[i] = True
 
-n = int(input())
-m = int(input())
-computer = [[] for _ in range(n+1)]
+def dfs(graph, v, visited):
+    visited[v] = True
 
-visited = [False] * (n+1)
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
 
-for i in range(m):
+
+
+def sortGraph(graph):
+    for i in range(1, N + 1):
+        graph[i].sort()
+    return graph
+
+
+N = int(input())
+M = int(input())
+
+graph = [[] for _ in range(N+1)]
+visited = [False] * (N+1)
+for _ in range(M):
     a, b = map(int, input().split())
-    computer[a].append(b)
-    computer[b].append(a)
-bfs(computer, 1, visited)
+    graph[a].append(b)
+    graph[b].append(a)
+
+graph = sortGraph(graph)
+dfs(graph, 1, visited)
 
 print(visited.count(True) - 1)
-
 
