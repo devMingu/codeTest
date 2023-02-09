03ef666272e9a97e39b6ba8e20570b8bdc1159ee
devMingu <rooster100@naver.com>
2023-02-09 15:03:27 +0900

    고리즘 풀이

--- 백준/트리.py
+++ 백준/트리.py
@@ -1,45 +1,22 @@
 import sys
 sys.setrecursionlimit(10**6)
-def dfs(graph, v):
-    global answer
-    for i in graph[v]:
-        if len(graph[i]) == 0:
-            answer -= 1
-        else:
-            dfs(graph, i)
+def dfs(node, delete_idx):
+    node[delete_idx] = -2
 
-    if len(graph[v]) == 0:
-        answer -= 1
-
-
-
+    for i in range(len(node)):
+        if delete_idx == node[i]:
+            dfs(node, i)
 
 N = int(input())
 node = list(map(int, input().split()))
 delete_node = int(input())
 
-graph = [[] for _ in range(N)]
-
-parent = set()
-node_idx = 0
-root = 0
-for el in node:
-    if el == -1:
-        root = node_idx
-        node_idx += 1
-        continue
-    graph[el].append(node_idx)
-    parent.add(el)
-    node_idx += 1
-
-answer = N - len(parent)
-# print(graph, answer)
-dfs(graph, delete_node)
-
-# print(graph[root], root)
-
-if len(graph[root]) - 1 == 0:
-    answer += 1
+answer = 0
+dfs(node, delete_node)
+for i in range(len(node)):
+    if node[i] != -2 and i not in node:
+        answer += 1
 
 print(answer)
+
 
