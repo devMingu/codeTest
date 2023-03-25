b8e1a797ee2a403adad2b36b8665aacf5a1a658c
devMingu <rooster100@naver.com>
2023-03-25 10:44:40 +0900

    알고리즘 풀이

--- 백준/숨바꼭질.py
+++ 백준/숨바꼭질.py
@@ -1,38 +1,34 @@
 from collections import deque
-def bfs(start):
-    queue = deque([start])
-    visited[start] = True
 
-    answer = [0] * (n + 1)
+def bfs(v):
+    visitied[v] = 1
+    q = deque([])
+    q.append(v)
 
-    answer[start] = 1
+    dx = [-1, 1, 2]
+    cnt = 0
+    while q:
+        v = q.popleft()
+        for i in range(3):
+            if dx[i] == 2:
+                nv = dx[i]*v
+            else:
+                nv = v + dx[i]
 
-    while queue:
-        v = queue.popleft()
-        for el in graph[v]:
-            if not visited[el]:
-                visited[el] = True
-                queue.append(el)
-                answer[el] = answer[v] + 1
+            if nv < 0 or nv >= max(x1, x2) + 10:
+                continue
 
-    return answer
+            if not visitied[nv]:
+                if nv == x2:
+                    visitied[nv] = visitied[v] + 1
+                    return visitied[x2]
+                visitied[nv] = visitied[v] + 1
+                q.append(nv)
 
-n, m = map(int, input().split())
+    return visitied[x2]
 
-graph = [[] for _ in range(n+1)]
-visited = [False] * (n+1)
+x1, x2 = map(int, input().split())
+visitied = [0 for _ in range(max(x1, x2) + 10)]
+answer = bfs(x1)
 
-for i in range(m):
-    x, y = map(int, input().split())
-    graph[x].append(y)
-    graph[y].append(x)
-    graph[x].sort()
-    graph[y].sort()
-
-
-result = bfs(1)
-
-max_length = max(result)
-print(result.index(max_length) ,max_length - 1, result.count(max_length))
-
-
+print(answer - 1)
