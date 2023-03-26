00af66f5121e2f2e3e6787d1d21dd407ba992ef9
devMingu <rooster100@naver.com>
2023-03-26 13:44:22 +0900

    알고리즘 풀이

--- 프로그래머스/숫자 변환하기.py
+++ 프로그래머스/숫자 변환하기.py
@@ -1,32 +1,40 @@
+from collections import deque
+
+
+def bfs(v, y, n, visited):
+    visited[v] = 1
+
+    q = deque([])
+    q.append(v)
+
+    dv = [n, 2, 3]
+
+    while q:
+        v = q.popleft()
+
+        for i in range(3):
+            if i == 0:
+                nv = v + dv[i]
+            else:
+                nv = v * dv[i]
+
+            if nv < 1 or nv > y:
+                continue
+
+            if not visited[nv]:
+                visited[nv] = visited[v] + 1
+                if nv == y:
+                    return visited[v]
+
+                q.append(nv)
+    return -1
+
+
 def solution(x, y, n):
     answer = 0
-
-    while y > x:
-        if y % 3 == 0 and y // 3 >= x and y % 2 == 0 and y // 2 >= x:
-            if y - n >= x:
-                y = min(y // 3, y // 2, y - n)
-            else:
-                y = min(y // 3, y // 2)
-
-        elif y % 3 == 0 and y // 3 >= x:
-            if y - n >= x:
-                y = min(y // 3, y - n)
-            else:
-                y //= 3
-
-        elif y % 2 == 0 and y // 2 >= x:
-            if y - n >= x:
-                y = min(y // 2, y - n)
-            else:
-                y //= 2
-        else:
-            y -= n
-
-        answer += 1
-
-    if y < x:
-        answer = -1
-
+    visited = [0 for _ in range(y + 1)]
+    if x == y:
+        answer = 0
+    else:
+        answer = bfs(x, y, n, visited)
     return answer
-
-# 실패
