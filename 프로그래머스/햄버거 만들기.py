a33e5264e2cd776859fb2b7a15640531691667db
devMingu <rooster100@naver.com>
2023-04-22 23:31:46 +0900

    알고리즘 풀이

--- 프로그래머스/햄버거 만들기.py
+++ 프로그래머스/햄버거 만들기.py
@@ -1,37 +1,27 @@
-def is_hamburgur(ingredient, start):
-    ok = [1, 2, 3, 1]
-
-    idx = 0
-    cnt = 0
-    for i in range(start, start + 4):
-        if ok[idx] == ingredient[i]:
-            cnt += 1
-        idx += 1
-
-    if cnt == 4:
-        while cnt:
-            ingredient.pop(start)
-            cnt -= 1
-
-        return [ingredient, 1]
-
-    return [ingredient, 0]
-
-
 def solution(ingredient):
     answer = 0
 
     start = 0
     n = len(ingredient)
+    ok = [1, 2, 3, 1]
+
+    one = []
 
     while start <= len(ingredient) - 4:
         if ingredient[start] == 1:
-            ingredient, result = is_hamburgur(ingredient, start)
-            if result:
-                answer += 1
-                start -= 1
-                continue
-            print(ingredient)
+            one.append(start)
+
+        if ingredient[start: start + 4] == ok:
+            answer += 1
+            for i in range(4):
+                ingredient.pop(start)
+
+            while one:
+                tmp = one.pop()
+                if tmp != start:
+                    start = tmp
+                    break
+            continue
 
         start += 1
 
