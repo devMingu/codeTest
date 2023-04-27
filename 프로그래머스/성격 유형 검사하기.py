2baa7b419539b7b7783eee687eb66d20785b3a3a
devMingu <rooster100@naver.com>
2023-04-27 14:26:37 +0900

    알고리즘 풀이

--- 프로그래머스/성격 유형 검사하기.py
+++ 프로그래머스/성격 유형 검사하기.py
@@ -1,50 +1,27 @@
-def survey_score(choice):
-    if choice >= 5:
-        if choice == 5:
-            return 1
-        elif choice == 6:
-            return 2
-        else:
-            return 3
-    if choice <= 3:
-        if choice == 1:
-            return 3
-        elif choice == 2:
-            return 2
-        else:
-            return 1
+# 코드 리팩토링
 
-    return 0
+def survey_score(choice):
+    return choice - 4 if choice >= 4 else 4 - choice
 
 
 def solution(survey, choices):
     answer = ''
-
+    type_index = ["RT", "CF", "JM", "AN"]
     idx = 0
-
     char_type = {}
-    for el in survey:
-        disagree_el = el[0]
-        agree_el = el[1]
-        choice = choices[idx]
 
-        idx += 1
+    for el in survey:
+        disagree_el, agree_el = el[0], el[1]
+        choice = choices[idx]
 
         score = survey_score(choice)
         if choice >= 5:
-            if agree_el not in char_type:
-                char_type[agree_el] = score
-            else:
-                char_type[agree_el] += score
-        if choice <= 3:
-            if disagree_el not in char_type:
-                char_type[disagree_el] = score
-            else:
-                char_type[disagree_el] += score
+            char_type[agree_el] = score if agree_el not in char_type else char_type[agree_el] + score
+        else:
+            char_type[disagree_el] = score if disagree_el not in char_type else char_type[disagree_el] + score
 
-    print(char_type)
+        idx += 1
 
-    type_index = ["RT", "CF", "JM", "AN"]
     for el in type_index:
         t1 = char_type[el[0]] if el[0] in char_type else 0
         t2 = char_type[el[1]] if el[1] in char_type else 0
@@ -52,3 +29,64 @@ def solution(survey, choices):
         answer += el[0] if t1 >= t2 else el[1]
 
     return answer
+
+
+
+
+
+
+
+# def survey_score(choice):
+#     if choice >= 5:
+#         if choice == 5:
+#             return 1
+#         elif choice == 6:
+#             return 2
+#         else:
+#             return 3
+#     if choice <= 3:
+#         if choice == 1:
+#             return 3
+#         elif choice == 2:
+#             return 2
+#         else:
+#             return 1
+#
+#     return 0
+#
+#
+# def solution(survey, choices):
+#     answer = ''
+#
+#     idx = 0
+#
+#     char_type = {}
+#     for el in survey:
+#         disagree_el = el[0]
+#         agree_el = el[1]
+#         choice = choices[idx]
+#
+#         idx += 1
+#
+#         score = survey_score(choice)
+#         if choice >= 5:
+#             if agree_el not in char_type:
+#                 char_type[agree_el] = score
+#             else:
+#                 char_type[agree_el] += score
+#         if choice <= 3:
+#             if disagree_el not in char_type:
+#                 char_type[disagree_el] = score
+#             else:
+#                 char_type[disagree_el] += score
+#
+#     print(char_type)
+#
+#     type_index = ["RT", "CF", "JM", "AN"]
+#     for el in type_index:
+#         t1 = char_type[el[0]] if el[0] in char_type else 0
+#         t2 = char_type[el[1]] if el[1] in char_type else 0
+#
+#         answer += el[0] if t1 >= t2 else el[1]
+#
+#     return answer
