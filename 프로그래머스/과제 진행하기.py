34f2feb4e594ac733e8c87998aff827a404f9d29
devMingu <rooster100@naver.com>
2023-05-05 17:52:12 +0900

    알고리즘 풀이

--- 프로그래머스/과제 진행하기.py
+++ 프로그래머스/과제 진행하기.py
@@ -1,6 +1,5 @@
 import heapq
 
-
 def convert_hours_to_minutes(time):
     hours, minute = time.split(':')
 
@@ -9,10 +8,10 @@ def convert_hours_to_minutes(time):
 
     return hours * 60 + minute
 
-
 def solution(plans):
     answer = []
     h = []
+    wait = []
 
     for el in plans:
         lecture, start_time, how_long_time = el[0], el[1], el[2]
@@ -23,42 +22,50 @@ def solution(plans):
 
     print(h)
 
+    # 시작 시간을 결정
     start, end, lec = heapq.heappop(h)
 
-    tmp = []
-    while len(h):
-        next = h[0]
-        if end <= next[0]:
-            answer.append(lec)
-            if end == next[0]:  # 새로운 과제
-                start, end, lec = heapq.heappop(h)
-            else:
-                if tmp:  # 멈춘 과제가 있다면 -> 멈춘 과제 시작
-                    start, end, lec = heapq.heappop(tmp)
-                else:  # 멈춘 과제가 없다면 -> 새로운 과제 시작
-                    start, end, lec = heapq.heappop(h)
-            if len(h) == 0:
+    while h:
+        next = h[0] # 다음번 실행할 함수.
+
+        if end <= next[0]: # 다음번 과제보다 일찍 끝나거나 알맞게 끝나는 경우
+            # 알맞게 끝나는 경우 next를 실행
+            if end == next[0]:
                 answer.append(lec)
-        else:
-            heapq.heappush(tmp, [next[0], end, lec])
+
+            # 일찍 끝나는 경우 -> 남은 시간을 고려해서 대기상태에 있는 애들 실행.
+            else:
+                answer.append(lec)
+                # 남는 시간이 얾마인지 계산
+                remain_time = next[0] - end
+                while wait:
+                    if remain_time <= 0:
+                        break
+
+                    tmp = wait[-1]
+                    if tmp[0] <= remain_time:
+                        wait_t, wait_lec = wait.pop()
+                        answer.append(wait_lec)
+                        remain_time -= wait_t
+
+                    else:
+                        wait[-1][0] -= remain_time
+                        remain_time = 0
             start, end, lec = heapq.heappop(h)
+            if not h:
+                answer.append(lec)
+                break
 
-    while tmp:
-        tmp_s, tmp_e, tmp_lec = heapq.heappop(tmp)
-        answer.append(tmp_lec)
+        # 다 끝나지 않았는데 다음번 과제를 시작하는 경우
+        else:
+            wait.append([end-next[0], lec])
+            start, end, lec = heapq.heappop(h)
+            if not h:
+                answer.append(lec)
+                break
+
+    while wait:
+        a, b = wait.pop()
+        answer.append(b)
+
     return answer
-
-
-740
-780
-music
-750
-850
-computer
-760
-810
-science
-840
-870
-history
-
