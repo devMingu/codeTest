5e28df485172ba9c4ac460687006db4276f05e1c
devMingu <rooster100@naver.com>
2023-05-16 21:35:41 +0900

    알고리즘 문제풀이 BFS

---
+++ 백준/정수 a를 k로 만들기.py
@@ -0,0 +1,35 @@
+from collections import deque
+
+def bfs():
+    q = deque()
+    q.append(A)
+
+
+
+    while q:
+        v = q.popleft()
+        for i in range(2):
+            if i == 0: # 1더하기
+                if visited[v+1] == 0:
+                    visited[v+1] = visited[v] + 1
+                    q.append(v+1)
+            else: # 2곱하기
+                if v*2 <= K:
+                    if visited[v*2] == 0:
+                        visited[v*2] = visited[v] + 1
+                        q.append(v*2)
+
+        if visited[K] != 0:
+            print(visited[K])
+            break
+
+
+
+
+
+A, K = map(int, input().split())
+
+visited = [0 for _ in range(1000001)]
+
+bfs()
+
